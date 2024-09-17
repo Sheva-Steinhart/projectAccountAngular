5d30b69aaa6618e13f9bf190aad216d56b60c0fe
shevy <your-email@example.com>
2024-09-17 12:25:39 +0300

    first

---
+++ angular/src/app/components/customer-receipts/customer-receipts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-receipts',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-receipts.component.html',
+  styleUrl: './customer-receipts.component.scss'
+})
+export class CustomerReceiptsComponent {
+
+}
